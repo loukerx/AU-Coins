e9057bcb0d1d5885aa72480e96101be4280ec58d
Yin Hua <yinh@Yins-MacBook-Pro.local>
2015-02-15 11:27:11 +1100

    Initial Commit

---
+++ AU Coins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AU Coins
+//
+//  Created by Yin Hua on 15/02/2015.
+//  Copyright (c) 2015 Yin Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AU Coins/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AU Coins
+//
+//  Created by Yin Hua on 15/02/2015.
+//  Copyright (c) 2015 Yin Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
