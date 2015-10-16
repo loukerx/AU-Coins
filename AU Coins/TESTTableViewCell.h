15f6f9c39cf7cce2944f5886f733f7311a5d6939
Yin Hua <loukerx@gmail.com>
2015-10-16 14:41:57 +1100

    branch
    aaaaa

---
+++ AU Coins/TESTTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TESTTableViewCell.h
+//  AU Coins
+//
+//  Created by Yin Hua on 16/10/2015.
+//  Copyright © 2015 Yin Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TESTTableViewCell : UITableViewCell
+
+@end
---
+++ AU Coins/TESTTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TESTTableViewCell.m
+//  AU Coins
+//
+//  Created by Yin Hua on 16/10/2015.
+//  Copyright © 2015 Yin Hua. All rights reserved.
+//
+
+#import "TESTTableViewCell.h"
+
+@implementation TESTTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
