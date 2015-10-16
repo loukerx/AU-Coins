b8f570f4c28901cb294628d2b51b6f26ee7c7203
Yin Hua <loukerx@gmail.com>
2015-10-16 14:43:43 +1100

    aaaa a

---
+++ AU Coins/AboutTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutTableViewController.h
+//  AU Coins
+//
+//  Created by Yin Hua on 16/10/2015.
+//  Copyright © 2015 Yin Hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutTableViewController : UITableViewController
+
+@end
